3d912cf04e1376726c6abd7a672655097159e6c6
frissyn <62220201+frissyn@users.noreply.github.com>
2022-06-02 17:29:44 +0000

    :sparkles: add index page and default layout

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page">
+    <div class="navbar bg-base-100 shadow">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost btn-circle">
+            <IconDropdown />
+          </label>
+          <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow-lg bg-base-100 rounded-box w-52">
+            <li><a href="/"><IconHome /> Homepage</a></li>
+            <li><a href="/"><IconBriefcase /> Portfolio</a></li>
+            <li><a href="https://github.com/frissyn/frissyn"><IconLogoGitHub /> View Source</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <a class="btn btn-ghost normal-case text-xl">frissyn.</a>
+      </div>
+      <div class="navbar-end">
+        <a class="btn btn-ghost" title="Instagram"><IconLogoInstagram /></a>
+        <a class="btn btn-ghost" title="Twitter"><IconLogoTwitter /></a>
+        <a class="btn btn-ghost" title="GitHub"><IconLogoGitHub /></a>
+      </div>
+    </div>
+
+    <slot />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hero min-h-[95vh] max-h-screen" style="background-image: url(/images/wha-hero.jpg);">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Hello there...</h1>
+        <p class="mb-5">
+          Provident cupiditate voluptatem et in.
+          Quaerat fugiat ut assumenda excepturi exercitationem quasi.
+          In deleniti eaque aut repudiandae et a id nisi.
+        </p>
+        <div class="marquee">
+          <div class="marquee-content">
+            <p class="py-6">
+              Software Development // Graphic Design // Motion Design // Illustration //&nbsp;
+            </p>
+            <p class="py-6">
+              Software Development // Graphic Design // Motion Design // Illustration //&nbsp;
+            </p>
+          </div>
+        </div>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  definePageMeta({ layout: 'default' })
+</script>
